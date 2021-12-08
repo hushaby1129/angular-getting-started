fbb0194996e6d668e5abc1706cf7095a8f2e6eb4
hushaby1129 <36461935+hushaby1129@users.noreply.github.com>
2021-12-08 16:01:29 +0900

    // 2021.12.08 실습 안료

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,17 +1,34 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms'
+
 import { CartService } from "../cart.service";
 
 @Component({
-  selector: 'app-cart',
-  templateUrl: './cart.component.html',
-  styleUrls: ['./cart.component.css']
+    selector: 'app-cart',
+    templateUrl: './cart.component.html',
+    styleUrls: ['./cart.component.css']
 })
 export class CartComponent {
 
-  items = this.cartService.getItems();
+    items = this.cartService.getItems();
 
-  constructor(
-    private cartService: CartService
-  ) { }
+    checkoutForm = this.formBuilder.group({
+        name: ""
+        , address: ""
+
+    });
+
+    constructor(
+        private cartService: CartService
+        , private formBuilder: FormBuilder
+    ) { }
+
+    onSubmit(): void {
+        this.items = this.cartService.clearCart();
+        console.warn("주문 완료", this.checkoutForm.value);
+        debugger;
+
+        this.checkoutForm.reset();
+    }
 
 }
--- src/app/shipping/shipping.component.ts
+++ src/app/shipping/shipping.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { CartService } from "../cart.service";
 
 @Component({
   selector: 'app-shipping',
@@ -7,6 +8,10 @@ import { Component } from '@angular/core';
 })
 export class ShippingComponent {
 
-  constructor() { }
+  shippingCosts = this.cartService.getShippingPrices();
+
+  constructor(
+      private cartService: CartService
+  ) { }
 
 }
